5f9e7d85e06718e25d6d87d2043389300feb155f
Kasper <kasper.berg1@hotmail.com>
2023-03-28 00:04:10 +0200

    Merge conflict or something
    Accepted Route to /

--- ageofus/src/App.js
+++ ageofus/src/App.js
@@ -1,22 +1,15 @@
 import { React } from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-<<<<<<< Updated upstream
-=======
 import Home from './pages/home.js'
->>>>>>> Stashed changes
 
 function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< Updated upstream
-        
-=======
         <Route exact path='/'element={<Home></Home>}>
           
         </Route>
 
->>>>>>> Stashed changes
       </Routes>
     </Router>
   );
