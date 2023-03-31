a06a769433a1699c7cdbcaae02d9ebaa1eeaea75
Kasper <kasper.berg1@hotmail.com>
2023-03-31 12:03:44 +0200

    Display prices
    Added a <p> to display prices on each character button

--- src/components/CharacterMenu.js
+++ src/components/CharacterMenu.js
@@ -14,16 +14,19 @@ function CharacterMenu (){
                 <li>
                     <button onClick={() => handleClick(1, 15)}>
                         <img src={Character1} alt="Character 1" />
+                        <p>15</p>
                     </button>
                 </li>
                 <li>
                     <button onClick={() => handleClick(2, 50)}>
                         <img src={Character2} alt="Character 2" />
+                        <p>50</p>
                     </button>
                 </li>
                 <li>
                     <button onClick={() => handleClick(3, 100)}>
                         <img src={Character3} alt="Character 3" />
+                        <p>100</p>
                     </button>
                 </li>
             </ul>
