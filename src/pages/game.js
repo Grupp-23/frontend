5d6cf74901575092caa7947f2d17de76a67d8b11
Oliver Bengtsson <58479844+LongeMan@users.noreply.github.com>
2023-04-06 14:06:41 +0200

    Added music for game
    Co-Authored-By: Ludvig Larsson <127208421+LVL270@users.noreply.github.com>

--- src/pages/game.js
+++ src/pages/game.js
@@ -3,10 +3,10 @@ import GameView from "../components/GameView";
 import CharacterMenu from "../components/CharacterMenu";
 
 
+
 function Game (){
     return (
         <main>
-        
             <GameView placeholder="Name"></GameView>
             
             <CharacterMenu />
