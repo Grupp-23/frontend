e4d27bd1ffa9a21771f5cd35ae9de1597df951a0
Oliver Bengtsson <58479844+LongeMan@users.noreply.github.com>
2023-04-06 12:16:24 +0200

    Added two bases to the game
    We have tried to add two bases to the gameview. This is still in progress.
    
    Co-Authored-By: Ludvig Larsson <127208421+LVL270@users.noreply.github.com>

--- src/components/GameView.js
+++ src/components/GameView.js
@@ -1,9 +1,12 @@
 import "./GameView.css"
+import GameBaseFirst from "../components/GameBaseFirst";
+import GameBaseSecond from "../components/GameBaseSecond";
 
 function GameView (){
     return (
         <div id="gameview">
-            
+            <GameBaseFirst/>
+            <GameBaseSecond></GameBaseSecond>
         </div>
     );
 }
