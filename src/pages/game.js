f1b1900d6d7f2f1a1cf152cee8fccb13531f4abc
Oliver Bengtsson <58479844+LongeMan@users.noreply.github.com>
2023-04-06 10:35:45 +0200

    Added a picture for base
    Co-Authored-By: Ludvig Larsson <127208421+LVL270@users.noreply.github.com>

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,9 +1,11 @@
 import HomeMenu from "../components/HomeMenu"
 import GameView from "../components/GameView";
+import GameBase from "../components/GameBase";
 
 function Game (){
     return (
         <main>
+            
             <GameView placeholder="Name"></GameView>
         </main>
     );
