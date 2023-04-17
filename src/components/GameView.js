05bf84a8dbaddb403f9039ee477556f1d15504e1
Oliver Bengtsson <58479844+LongeMan@users.noreply.github.com>
2023-04-17 15:42:41 +0200

    Started to add a two div-blocks for the window to scroll when mouse is near them.

--- src/components/GameView.js
+++ src/components/GameView.js
@@ -2,14 +2,22 @@ import "./GameView.css"
 import GameBaseFirst from "../components/GameBaseFirst";
 import GameBaseSecond from "../components/GameBaseSecond";
 
+
 function GameView (){
+
+    
+
     return (
+        
         <div id="gameview">
             <GameBaseFirst/>
-            
             <GameBaseSecond></GameBaseSecond>
+            <div className="scroll-left"></div>
+            <div className="scroll-right"></div>
+            
         </div>
-    );
-}
+    )
+
+    }
 
 export default GameView;
