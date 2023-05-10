185435cfc2946f15ccd82e861657b9faafed7c5e
Kasper <kasper.berg1@hotmail.com>
2023-05-10 15:08:45 +0200

    Added documentation

--- src/services/SocketClient.js
+++ src/services/SocketClient.js
@@ -1,9 +1,17 @@
-const SocketClient = {
+/**
+ * Represents the Socket Client, handles outgoing and incoming messages. Also saves the connection to the server.
+ * @constructor
+ */
 
+const SocketClient = {
     gameViewUpdate: null,
     client: null,
     navigate: null,
 
+    /**
+     * Establishes the connection to the server and saves is as client.
+     * @param {function} navigate - The navigation function to navigate to other paths on the website.
+     */
     setupConnection(navigate) {
         this.client = new WebSocket("ws://localhost:25565");
         this.client.onmessage = this.handleMessage.bind(this);
@@ -20,10 +28,18 @@ const SocketClient = {
         }
     },
 
+    /**
+     * Sends a message to the server.
+     * @param {string} message - The message to send.
+     */
     sendMessage(message) {
         this.client.send(message);
     },
 
+    /**
+     * Runs when a message has been collected.
+     * @param {object} event - The event object.
+     */
     handleMessage(event) {
         if (event.data === "found") {
             this.navigate("/game");
@@ -34,6 +50,10 @@ const SocketClient = {
         }
     },
 
+    /**
+     * Saves the update function from GameView.
+     * @param {function} update - The update function.
+     */
     saveUpdate(update) {
         this.gameViewUpdate = update;
     }
