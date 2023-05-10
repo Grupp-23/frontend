94ff262fb9b03ab5de89597038b423245dd19073
Kasper <kasper.berg1@hotmail.com>
2023-05-10 15:38:20 +0200

    Added documentation to game.js

--- src/pages/game.js
+++ src/pages/game.js
@@ -2,6 +2,9 @@ import GameView from "../components/GameView";
 import IngameMenu from "../components/IngameMenu";
 import ScrollFunction from "../components/ScrollFunction";
 
+/**
+ * Represents the Game Page
+ */
 function Game (){
     return (
         <div>
