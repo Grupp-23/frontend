6cb796e3d27899c815df8d0e5f751e0af6b98129
Kasper <kasper.berg1@hotmail.com>
2023-05-11 19:08:24 +0200

    Added the choice of character type
    Added the feature to spawn more than only 1 character type, also inverted the character images from the right side.

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import "./Character.css";
-import Swordsman from "../assets/swordsman.png";
+import CharacterType1 from "../assets/swordsman.png";
+import CharacterType2 from "../assets/archer.png";
+import CharacterType3 from "../assets/rider.png";
 
 /**
  * Renders a character.
  * @component
  * @param {string} characterId - The ID of the character.
  * @param {number} position - The position of the character.
+ * @param {number} characterType - The type of image to display.
  */
 class Character extends React.Component {
   render() {
-    const { characterId, position } = this.props;
+    const { characterId, position, characterType } = this.props;
+    let image = CharacterType1;
+    if (characterType === 2) {
+      image = CharacterType2;
+    }
+    else if (characterType === 3) {
+      image = CharacterType3
+    }
     return (
       <div className="character" id={characterId} style={{ left: `${position}%` }}>
-        <img src={Swordsman} alt="Character" />
+        <img src={image} alt="Character" />
       </div>
     );
   }
