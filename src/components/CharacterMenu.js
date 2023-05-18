63fce5893147c08e66bf33234ced20dfb1c5abcf
Enidsabotic <120934338+Enidsabotic@users.noreply.github.com>
2023-05-18 14:31:35 +0200

    coin pic added

--- src/components/CharacterMenu.js
+++ src/components/CharacterMenu.js
@@ -1,9 +1,10 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Character1 from "../assets/character1.png";
 import Character2 from "../assets/character2.png";
 import Character3 from "../assets/character3.png";
+import CoinPic from "../assets/goldpic.png";
 import "./CharacterMenu.css"
-import Socket from "../services/socket";
+import SocketClient from "../services/SocketClient";
 
 function CharacterMenu (){
     const progressRef = useRef(null);
@@ -14,24 +15,36 @@ function CharacterMenu (){
     const queue4 = useRef(null);
     const queue5 = useRef(null);
     const [queueCount, setQueueCount] = useState(0);
+    const [Money, setMoney] = useState(1);
+    const [buttonsDisabled, setButtonsDisabled] = useState(false);
+    const [isProgressBarVisible, setProgressBarVisible] = useState(false);
+    const [isQueueEmpty, setQueueEmpty] = useState(true);
 
     function handleClick(type) {
         Socket.trySpawnCharacter(type);
         let updatedQueueCount = queueCount + 1;
         setQueueCount(updatedQueueCount);
         
-        if (progressRef.current) {
-            progressRef.current.style.animation = "none";
-            setTimeout(() => {
-              progressRef.current.style.animation = "loading 2s";
-            }, 2);
-        }
 
         let queueList = [queue1, queue2, queue3, queue4, queue5];
         if (updatedQueueCount <= queueList.length) {
             queueList[updatedQueueCount-1].current.style.backgroundColor = 'red';
         }
 
+        
+        // Sends to the Server that we want to spawn a character
+        SocketClient.sendMessage(JSON.stringify({"method":"spawn","type":type}));
+
+        if (updatedQueueCount === queueList.length) {
+            setButtonsDisabled(true);
+        }
+
+        if (isQueueEmpty) {
+            setProgressBarVisible(true);
+            if (progressRef.current) {
+                progressRef.current.style.animation = "loading 2s";
+            }
+        }
     }
 
     function handleLoadingComplete() {
@@ -53,48 +66,63 @@ function CharacterMenu (){
             }
         }
         setQueueCount(updatedQueueCount);
+        setButtonsDisabled(false);
+
+        if (updatedQueueCount === 0) {
+            if (progressRef.current) {
+                progressRef.current.style.animation = "none";
+            }
+            setProgressBarVisible(false);
+            setQueueEmpty(true);
+        }
     }
+
+    useEffect(() => {
+        setQueueEmpty(queueCount === 0);
+    }, [queueCount]);
 
     return (
         <div className="menu" id="character-menu">
             <div>
                 <section className="container text-center">
-                    <h1>Spawning units...</h1>
-                    <div className="progress-bar" ref={progressBarRef}>
+                    <div class="Coin">
+                        <img class="coinpic" src={CoinPic} alt="coinpic" />
+                        <h1 class="coinAmount">{Money}</h1>
+                    </div>
+                    <h1 class="spawnText">Spawning units...</h1>
+                    <div className={`progress-bar ${isProgressBarVisible ? 'visible' : ''}`} ref={progressBarRef}>
                         <div className="progress" ref={progressRef} onAnimationEnd={handleLoadingComplete}>
                             <div className="progress-shadow"></div>
                         </div>
-                       
-                       <div class="queue">
-                           <div class='queue1' ref={queue1}></div>
-                           <div class='queue2' ref={queue2}></div>
-                           <div class='queue3' ref={queue3}></div>
-                           <div class='queue4' ref={queue4}></div>
-                           <div class='queue5' ref={queue5}></div>
-                       </div>
+                        <div class="queue">
+                            <div class='queue1' ref={queue1}></div>
+                            <div class='queue2' ref={queue2}></div>
+                            <div class='queue3' ref={queue3}></div>
+                            <div class='queue4' ref={queue4}></div>
+                            <div class='queue5' ref={queue5}></div>
+                        </div>
                     </div>
                 </section>
             </div>
             <ul>
                 <li>
-                    <button onClick={() => handleClick(1)}>
+                    <button onClick={() => handleClick(1)} disabled={buttonsDisabled}>
                         <img src={Character1} alt="Character 1" />
                         <p>15</p>
                     </button>
                 </li>
                 <li>
-                    <button onClick={() => handleClick(2)}>
+                    <button onClick={() => handleClick(2)} disabled={buttonsDisabled}>
                         <img src={Character2} alt="Character 2" />
                         <p>50</p>
                     </button>
                 </li>
                 <li>
-                    <button onClick={() => handleClick(3)}>
+                    <button onClick={() => handleClick(3)} disabled={buttonsDisabled}>
                         <img src={Character3} alt="Character 3" />
                         <p>100</p>
                     </button>
                 </li>
-
             </ul>
         </div>
     );
